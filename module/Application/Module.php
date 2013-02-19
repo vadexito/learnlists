46986abff9e2d0d03e058f206b545029557bd3a0
vadexito <david.mizrahi@m4x.org>
2013-02-19 20:22:12 +0100

    add default role through event listener

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -16,12 +16,27 @@ class Module
 {
     public function onBootstrap(MvcEvent $e)
     {
-        $e->getApplication()->getServiceManager()->get('translator');
-        $eventManager        = $e->getApplication()->getEventManager();
+        $services = $e->getApplication()->getServiceManager();
+        
+        //initiate translator
+        $services->get('translator');
+        $translatorEventManager = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
+        $moduleRouteListener->attach($translatorEventManager);
+        
+        //initiate default role after registering
+        $zfcuserService = $services->get('zfcuser_user_service');
+        $zfcuserEventManager = $zfcuserService->getEventManager();
+        $zfcuserEventManager->attach('register', function($e) use ($services) {
+            
+            $entityManager = $services->get('Doctrine\ORM\EntityManager');
+            $role = $entityManager->getRepository('Application\Entity\Role')->find(5);
+            $e->getParam('user')->addRole($role);
+        });
     }
 
+    
+    
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
