118df5648be5f88f1908f1d6c616609a57513fc0
vadexito <david.mizrahi@m4x.org>
2013-04-30 13:34:15 +0200

    new module rememberme docrtine ORM

---
+++ module/GoalioRememberMeDoctrineORM/src/GoalioRememberMeDoctrineORM/Entity/RememberMe.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GoalioRememberMeDoctrineORM\Entity;
+
+use GoalioRememberMe\Entity\RememberMe as GoalioRememberMeEntity;
+
+class RememberMe extends GoalioRememberMeEntity
+{
+    
+    
+}
