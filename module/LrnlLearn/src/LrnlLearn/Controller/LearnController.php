398fc718c41f272d37abccce916b0979bc0dfad4
vadexito <david.mizrahi@m4x.org>
2013-05-12 17:05:57 +0200

    add possibility of learn/ route

--- module/LrnlLearn/src/LrnlLearn/Controller/LearnController.php
+++ module/LrnlLearn/src/LrnlLearn/Controller/LearnController.php
@@ -10,13 +10,22 @@ class LearnController extends AbstractActionController
     {
         $id = (int) $this->params()->fromRoute('id', 0);
         if (!$id) {
-            return $this->redirect()->toRoute('home');
+            if ($this->zfcUserAuthentication()->hasIdentity()){
+                return $this->redirect()->toRoute('learn/summary');
+            } else {
+                return $this->redirect()->toRoute('zfcuser/login');
+            }
         }
         
         return [
             'listId'    => $id,
             'timePerQuestion' => 15
         ];
+    }
+    
+    public function summaryAction()
+    {
+        return [];
     }
 }
 
